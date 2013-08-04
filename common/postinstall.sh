d434e03683e3f26d16d3dfe38afaeac8d7ab0997
Nick Charlton <hello@nickcharlton.net>
2013-08-04 03:13:07 +0100

    Adds a common postinstall file for Vagrant boxes.

---
+++ common/postinstall.sh
@@ -0,0 +1,28 @@
+# postinstall.sh based upon Mitchell's old basebox example
+
+# mark the build time
+date > /etc/vagrant_box_build_time
+
+# update the apt cache and packages
+apt-get -qy update
+apt-get -qy upgrade
+apt-get clean
+
+# configure password-less sudo
+usermod -a -G sudo vagrant
+echo "%vagrant ALL=NOPASSWD:ALL" > /tmp/vagrant
+mv /tmp/vagrant /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+
+# install the vagrant-provided ssh keys
+mkdir -pm 700 /home/vagrant/.ssh
+curl -Lo /home/vagrant/.ssh/authorized_keys \
+  'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# clean up any artifacts
+rm -f /home/vagrant/shutdown.sh
+rm -f /home/vagrant/VBoxGuestAdditions.iso
+
+exit
