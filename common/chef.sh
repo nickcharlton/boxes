d2d5a983db2c96ac072ca8ae5311ec400787ac78
Nick Charlton <hello@nickcharlton.net>
2013-09-15 17:17:35 +0100

    Fixes a Chef dependency issue.

--- common/chef.sh
+++ common/chef.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Install Chef
+# see: http://stackoverflow.com/a/14738173/83386
+gem install net-ssh -v 2.2.2 --no-ri --no-rdoc
+gem install net-ssh-gateway -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
+gem install net-ssh-multi -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
 gem install chef --no-ri --no-rdoc
