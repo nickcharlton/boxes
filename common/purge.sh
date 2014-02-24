ca3231f54be2a9945c30584d4d6a712f966b2ec6
Nick Charlton <hello@nickcharlton.net>
2014-02-24 19:50:18 +0000

    Removes the unused count variables.

--- common/purge.sh
+++ common/purge.sh
@@ -41,13 +41,13 @@ rm -f /EMPTY
 
 # whiteout root
 echo "Cleaning up /..."
-count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`; 
+#count=`df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}'`; 
 dd if=/dev/zero of=/tmp/whitespace bs=1024;
 rm /tmp/whitespace;
   
 # whiteout /boot
 echo "Cleaning up /boot..."
-count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
+#count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 dd if=/dev/zero of=/boot/whitespace bs=1024;
 rm /boot/whitespace;
 
