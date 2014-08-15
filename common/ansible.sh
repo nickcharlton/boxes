dcc4a271fa317694e5331749521be81a818d7420
Daniel Groves <hello@danielgroves.net>
2014-08-15 18:27:05 +0100

    Cleans up Ansible installer

--- common/ansible.sh
+++ common/ansible.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# From source
-#git clone git://github.com/ansible/ansible.git
-#cd ./ansible
-#source ./hacking/env-setup
-
-# PIP installer
-#sudo easy_install pip
-#sudo pip install ansible
-
-# RPM installer
-#sudo yum install ansible
-
-# Apt installer
 apt-add-repository ppa:ansible/ansible
 apt-get -qy update
 apt-get -qy install ansible
