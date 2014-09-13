e12bdca76996f02eb0248ffc2ea500f344712145
Daniel Groves <hello@danielgroves.net>
2014-09-13 17:51:28 +0100

    Wheezy support

--- common/ansible.sh
+++ common/ansible.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-apt-add-repository ppa:ansible/ansible
-apt-get -qy update
-apt-get -qy install ansible
+case $(lsb_release -cs) in
+    'wheezy')
+        easy_install pip
+        pip install ansible
+    ;;
+    *)
+        apt-add-repository ppa:ansible/ansible
+        apt-get -qy update
+        apt-get -qy install ansible
+    ;;
+esac
