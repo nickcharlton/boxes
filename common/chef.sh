944701dda3645df19163d04b242eb53790756ce9
Nick Charlton <hello@nickcharlton.net>
2014-12-13 14:11:38 +0000

    Fixes the Chef to an older v11.16.4.

--- common/chef.sh
+++ common/chef.sh
@@ -5,4 +5,4 @@
 gem install net-ssh -v 2.2.2 --no-ri --no-rdoc
 gem install net-ssh-gateway -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
 gem install net-ssh-multi -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
-gem install chef --no-ri --no-rdoc
+gem install chef -v 11.16.4 --no-ri --no-rdoc
