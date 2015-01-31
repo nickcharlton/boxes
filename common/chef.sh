d418c02d500487da522127c58154fb99ef8bb510
Nick Charlton <nick@nickcharlton.net>
2015-01-31 12:54:17 +0000

    Switches Chef to use the metadata service and latest deb.

--- common/chef.sh
+++ common/chef.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-# Install Chef
-# see: http://stackoverflow.com/a/14738173/83386
-gem install net-ssh -v 2.2.2 --no-ri --no-rdoc
-gem install net-ssh-gateway -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
-gem install net-ssh-multi -v 1.1.0 --no-ri --no-rdoc --ignore-dependencies
-gem install chef -v 11.16.4 --no-ri --no-rdoc
+# Get the Chef package version through their metadata service.
+platform=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
+release=$(lsb_release -sr)
+version_url="https://www.opscode.com/chef/metadata?v=&prerelease=false&nightlies=false&p=$platform&pv=$release&m=x86_64"
+current_version=$(curl -s "$version_url")
+
+version_url=$(echo "$current_version" | awk '/url/{print $2}')
+version_sha=$(echo "$current_version" | awk '/sha256/{print $2}')
+
+# fetch chef
+curl $version_url -o chef.deb
+
+# check the file
+echo "$version_sha  chef.deb" > '/tmp/chef-checksum'
+shasum -a 256 -c '/tmp/chef-checksum'
+if [ $? -ne 0 ]; then
+    echo "Downloaded Chef package failed to checksum."
+    exit 1
+fi
+
+# install
+dpkg -i chef.deb
+
+# cleanup
+rm chef.deb
+rm /tmp/chef-checksum
