23c5ca210961d12aca467ec755867573a6e714bb
Nick Charlton <nick@nickcharlton.net>
2015-11-03 15:04:19 +0000

    Switch to open-vm-tools for VMWare. Closes #20.

--- scripts/vmtools.sh
+++ scripts/vmtools.sh
@@ -11,20 +11,13 @@ case $PACKER_BUILDER_TYPE in
         mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
         sh /mnt/VBoxLinuxAdditions.run
         umount /mnt
-        
+
         rm -f /home/vagrant/VBoxGuestAdditions.iso
     ;;
     'vmware-iso')
         echo "Installing VMware Tools..."
-        mkdir -p /mnt/cdrom
-        mount -o loop /home/vagrant/linux.iso /mnt/cdrom
 
-        cd /tmp
-        tar -zxpf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-        /tmp/vmware-tools-distrib/vmware-install.pl -d
-
-        umount /mnt/cdrom
-        rm -f /home/vagrant/linux.iso
+        apt-get -qy install open-vm-tools
     ;;
     *)
         printf "Nothing to do for the %s builder type.\n" $PACKER_BUILDER_TYPE
