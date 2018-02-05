fcfd0ca98db7785420c7fa5925d9f3b0602eba86
Nick Charlton <nick@nickcharlton.net>
2018-02-05 21:43:44 +0000

    Improve the default help.
    * Uses the new tagline.
    * Adds an acceptance test to showing the default help.

--- lib/boxes/command.rb
+++ lib/boxes/command.rb
@@ -1,12 +1,12 @@
 module Boxes
-  # Class which encapsulates the command line handling.
   class Command < CLAide::Command
-    require 'boxes/command/build'
-    require 'boxes/command/env'
+    require "boxes/command/build"
+    require "boxes/command/env"
 
     self.abstract_command = true
-    self.command = 'boxes'
+    self.command = "boxes"
     self.version = VERSION
-    self.description = 'Toolkit for building Vagrantboxes, VM and cloud images.'
+    self.description = "A command line tool that takes the complexity out " \
+                       "of building Vagrant boxes."
   end
 end
