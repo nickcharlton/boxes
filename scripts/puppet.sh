9be7fb045a44d3cafe33e4356e1d61532a1ed6c1
Nick Charlton <nick@nickcharlton.net>
2018-02-08 20:29:51 +0000

    Fix Puppet deb installation.
    * Puppetlabs -> Puppet.
    * Install the 5 series release.
    * Don't also install from the default apt repos.

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# see: http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html
+# see: https://docs.puppet.com/puppet/latest/puppet_platform.html
 
 # determine the os release
 os_release=$(lsb_release -cs)
 
 # configure the puppet package sources
-wget http://apt.puppetlabs.com/puppetlabs-release-$os_release.deb
-dpkg -i puppetlabs-release-$os_release.deb
+wget "https://apt.puppet.com/puppet5-release-$os_release.deb"
+dpkg -i "puppet5-release-$os_release.deb"
 apt-get -q update
-
-# install puppet
-apt-get install -qy puppet
